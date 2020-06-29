3aca51bb27d501aa02e8b2a03600ba8eda158eb0
Morallesss <morallesss@gmail.com>
2020-06-29 13:26:44 +0200

    Moved responsibilities to separate methods.

--- SOLID.SRP/BookingService.cs
+++ SOLID.SRP/BookingService.cs
@@ -5,15 +5,77 @@ namespace SOLID.SRP
 {
     internal class BookingService
     {
-        private string DirectoryPath = @"E:\Code\DevEnv\SOLID";
         public bool CreateOrder(Person person, Product product)
         {
-            if (person.Age < 18)
+            ValidateAgeOfPerson(person);
+
+            if (person.Money > 0)
             {
-                Console.WriteLine("Too young to buy!!!!");
-                throw new Exception();
+                PersonOder order = CreatePersonOrder(person, product);
+                SaveOrderInformation(person, order);
+                return true;
+            }
+            return false;
+        }
+
+        private void SaveOrderInformation(Person person, PersonOder order)
+        {
+            var date = DateTime.Now;
+            string currentPath = CreateFolderIfDoesNotExist(date);
+
+            SaveOrderInfo(person, order, currentPath);
+        }
+
+        private static void SaveOrderInfo(Person person, PersonOder order, string currentPath)
+        {
+            string orderInfo = $"{order.Person.Id}-{order.Product}-{order.Price}";
+            Console.WriteLine($"Order Info: {orderInfo}");
+            File.WriteAllText(Path.Combine(currentPath, $"{person.Id}.txt"), orderInfo);
+        }
+
+        private string CreateFolderIfDoesNotExist(DateTime date)
+        {
+            string DirectoryPath = @"E:\Code\DevEnv\SOLID";
+            var currentPath = Path.Combine(DirectoryPath, $"{date:yyyy-dd-MM}");
+            if (!Directory.Exists(currentPath))
+            {
+                Directory.CreateDirectory(currentPath);
             }
 
+            return currentPath;
+        }
+
+        private static PersonOder CreatePersonOrder(Person person, Product product)
+        {
+            decimal basePrice = CalculateBasePrice(product);
+            int discount = CalculateDiscount(person);
+            return new PersonOder
+            {
+                Person = person,
+                Product = product,
+                Price = basePrice - basePrice * (discount / 100.0m)
+            };
+        }
+
+        private static int CalculateDiscount(Person person)
+        {
+            int discount = 0;
+            if (person.Age > 65)
+            {
+                discount = 30 + person.Age - 65;
+                Console.WriteLine($"Discounts for old: {discount}%");
+            }
+            else if (person.IsStudent)
+            {
+                discount = 25;
+                Console.WriteLine("Discounts for student: 25%");
+            }
+
+            return discount;
+        }
+
+        private static decimal CalculateBasePrice(Product product)
+        {
             decimal basePrice = 0.0m;
 
             switch (product)
@@ -29,40 +91,16 @@ namespace SOLID.SRP
                     break;
             }
 
-            int discount = 0;
-            if (person.Age > 65)
-            {
-                discount =  30 + person.Age - 65;
-                Console.WriteLine($"Discounts for old: {discount}%");
-            }
-            else if (person.IsStudent)
-            {
-                discount = 25;
-                Console.WriteLine("Discounts for student: 25%");
-            }
+            return basePrice;
+        }
 
-            if (person.Money > 0)
+        private static void ValidateAgeOfPerson(Person person)
+        {
+            if (person.Age < 18)
             {
-                var order = new PersonOder
-                {
-                    Person = person,
-                    Product = product,
-                    Price = basePrice -  basePrice * (discount / 100.0m)
-                };
-                
-                var date = DateTime.Now;
-                var currentPath = Path.Combine(DirectoryPath, $"{date:yyyy-dd-MM}");
-                if(!Directory.Exists(currentPath))
-                {
-                    Directory.CreateDirectory(currentPath);
-                }
-
-                string orderInfo = $"{order.Person.Id}-{order.Product}-{order.Price}";
-                Console.WriteLine($"Order Info: {orderInfo}");
-                File.WriteAllText(Path.Combine(currentPath, $"{person.Id}.txt"), orderInfo);
-                return true;
+                Console.WriteLine("Too young to buy!!!!");
+                throw new Exception();
             }
-            return false;
         }
     }
 }
